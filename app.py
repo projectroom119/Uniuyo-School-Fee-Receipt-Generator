acf193abdbb4e44c0d27a0a2dd387a8aa9e96a40
Projectroom <108718734+projectroom119@users.noreply.github.com>
2025-08-01 00:22:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,6 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for("login"))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()
+
